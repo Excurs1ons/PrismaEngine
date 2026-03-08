882003ccd41e60a0c06d2e7a3b0db3661645c615
JasonGu <1456970218@qq.com>
2026-03-08 15:37:45 +0800

    Fix Android build: change Game::Initialize return type to int

--- src/game/Game.cpp
+++ src/game/Game.cpp
@@ -1,13 +1,13 @@
 // dllmain.cpp : 定义 DLL 应用程序的入口点。
 #include "Game.h"
 
-#include "SceneManager.h"
-#include "TriangleExample.h"
 #include "Common.h"
 #include "IApplication.h"
+#include "SceneManager.h"
+#include "TriangleExample.h"
 #include "pch.h"
 
-bool Game::Initialize() {
+int Game::Initialize() {
     // 调用基类初始化
     if (!Application::Initialize()) {
         return false;
@@ -30,45 +30,45 @@ void Game::Shutdown() {
 // 动态库模式使用 Initialize/Run/Shutdown，静态库模式使用 Game_ 前缀
 extern "C" {
 #ifdef GAME_BUILD_SHARED
-    // 动态库模式 - 导出标准函数名
-    #if defined(_WIN32) || defined(_WIN64)
-        __declspec(dllexport)
-    #else
-        __attribute__((visibility("default")))
-    #endif
-    bool Initialize() {
-        return Game::GetInstance().Initialize();
-    }
-
-    #if defined(_WIN32) || defined(_WIN64)
-        __declspec(dllexport)
-    #else
-        __attribute__((visibility("default")))
-    #endif
-    int Run() {
-        return Game::GetInstance().Run();
-    }
-
-    #if defined(_WIN32) || defined(_WIN64)
-        __declspec(dllexport)
-    #else
-        __attribute__((visibility("default")))
-    #endif
-    void Shutdown() {
-        Game::GetInstance().Shutdown();
-    }
+// 动态库模式 - 导出标准函数名
+#if defined(_WIN32) || defined(_WIN64)
+__declspec(dllexport)
 #else
-    // 静态库模式 - 使用 Game_ 前缀避免符号冲突
-    bool Game_Initialize() {
-        return Game::GetInstance().Initialize();
-    }
+__attribute__((visibility("default")))
+#endif
+bool Initialize() {
+    return Game::GetInstance().Initialize();
+}
 
-    int Game_Run() {
-        return Game::GetInstance().Run();
-    }
+#if defined(_WIN32) || defined(_WIN64)
+__declspec(dllexport)
+#else
+__attribute__((visibility("default")))
+#endif
+int Run() {
+    return Game::GetInstance().Run();
+}
 
-    void Game_Shutdown() {
-        Game::GetInstance().Shutdown();
-    }
+#if defined(_WIN32) || defined(_WIN64)
+__declspec(dllexport)
+#else
+__attribute__((visibility("default")))
+#endif
+void Shutdown() {
+    Game::GetInstance().Shutdown();
+}
+#else
+// 静态库模式 - 使用 Game_ 前缀避免符号冲突
+bool Game_Initialize() {
+    return Game::GetInstance().Initialize();
+}
+
+int Game_Run() {
+    return Game::GetInstance().Run();
+}
+
+void Game_Shutdown() {
+    Game::GetInstance().Shutdown();
+}
 #endif
 }
