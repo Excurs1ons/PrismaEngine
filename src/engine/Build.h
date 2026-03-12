d5da17dff914f216e26a1dd65d95c4783b2fd844
JasonGu <1456970218@qq.com>
2026-03-12 14:30:27 +0000

    refactor(prune): simplify Build.h and remove test files
    - Simplify Build.h (only Windows x64)
    - Remove unused test.cpp files

--- src/engine/Build.h
+++ src/engine/Build.h
@@ -14,7 +14,6 @@
 #endif
 
 // 检测是否启用 ImGui Debug 工具
-// 这个宏由 CMake 定义 PRISMA_ENABLE_IMGUI_DEBUG
 #ifndef PRISMA_ENABLE_IMGUI_DEBUG
     #if PRISMA_DEBUG && WIN32
         #define PRISMA_ENABLE_IMGUI_DEBUG 1
@@ -23,40 +22,19 @@
     #endif
 #endif
 
-// 平台检测
+// 平台检测 - Prune分支仅支持Windows x64
 #if defined(_WIN32) || defined(_WIN64)
     #define PRISMA_PLATFORM_WINDOWS 1
+    #define PRISMA_PLATFORM_ANDROID 0
+    #define PRISMA_PLATFORM_LINUX 0
+    #define PRISMA_PLATFORM_WEB 0
+    #define PRISMA_PLATFORM_IOS 0
+    #define PRISMA_PLATFORM_MACOS 0
 #else
     #define PRISMA_PLATFORM_WINDOWS 0
-#endif
-
-#if defined(__ANDROID__)
-    #define PRISMA_PLATFORM_ANDROID 1
-#else
     #define PRISMA_PLATFORM_ANDROID 0
-#endif
-
-#if defined(__linux__)
-    #define PRISMA_PLATFORM_LINUX 1
-#else
     #define PRISMA_PLATFORM_LINUX 0
-#endif
-
-
-#if defined(__EMSCRIPTEN__)
-    #define PRISMA_PLATFORM_WEB 1
-#else
     #define PRISMA_PLATFORM_WEB 0
-#endif
-
-#if defined(__APPLE__)
-    #include <TargetConditionals.h>
-    #if TARGET_OS_IPHONE
-        #define PRISMA_PLATFORM_IOS 1
-    #else
-        #define PRISMA_PLATFORM_MACOS 1
-    #endif
-#else
     #define PRISMA_PLATFORM_IOS 0
     #define PRISMA_PLATFORM_MACOS 0
 #endif
