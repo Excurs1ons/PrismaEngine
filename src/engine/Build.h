0c8afa3a52b36baab5d0b25ed8c9e46f837d31de
JasonGu <1456970218@qq.com>
2026-03-13 02:44:58 +0000

    Simplify Build.h - direct defines

--- src/engine/Build.h
+++ src/engine/Build.h
@@ -1,40 +1,20 @@
 #pragma once
 
-// Build.h - 构建配置相关的预处理器宏定义
-
-// 检测 Debug 构建模式
-#if defined(_DEBUG) || defined(DEBUG)
-    #ifndef PRISMA_DEBUG
-        #define PRISMA_DEBUG 1
-    #endif
+#ifdef _DEBUG
+#define PRISMA_DEBUG 1
 #else
-    #ifndef PRISMA_DEBUG
-        #define PRISMA_DEBUG 0
-    #endif
+#define PRISMA_DEBUG 0
 #endif
 
-// 检测是否启用 ImGui Debug 工具
-#ifndef PRISMA_ENABLE_IMGUI_DEBUG
-    #if PRISMA_DEBUG && WIN32
-        #define PRISMA_ENABLE_IMGUI_DEBUG 1
-    #else
-        #define PRISMA_ENABLE_IMGUI_DEBUG 0
-    #endif
-#endif
-
-// 平台检测 - Prune分支仅支持Windows x64
-#if defined(_WIN32) || defined(_WIN64)
-    #define PRISMA_PLATFORM_WINDOWS 1
-    #define PRISMA_PLATFORM_ANDROID 0
-    #define PRISMA_PLATFORM_LINUX 0
-    #define PRISMA_PLATFORM_WEB 0
-    #define PRISMA_PLATFORM_IOS 0
-    #define PRISMA_PLATFORM_MACOS 0
+#if PRISMA_DEBUG && defined(_WIN32)
+#define PRISMA_ENABLE_IMGUI_DEBUG 1
 #else
-    #define PRISMA_PLATFORM_WINDOWS 0
-    #define PRISMA_PLATFORM_ANDROID 0
-    #define PRISMA_PLATFORM_LINUX 0
-    #define PRISMA_PLATFORM_WEB 0
-    #define PRISMA_PLATFORM_IOS 0
-    #define PRISMA_PLATFORM_MACOS 0
+#define PRISMA_ENABLE_IMGUI_DEBUG 0
 #endif
+
+#define PRISMA_PLATFORM_WINDOWS 1
+#define PRISMA_PLATFORM_ANDROID 0
+#define PRISMA_PLATFORM_LINUX 0
+#define PRISMA_PLATFORM_WEB 0
+#define PRISMA_PLATFORM_IOS 0
+#define PRISMA_PLATFORM_MACOS 0
