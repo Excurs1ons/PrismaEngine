059a195a6004e634ab3709d986d2c5c9e855cb28
Excurs1ons <1456970218@qq.com>
2026-05-14 09:51:02 +0000

    feat(render): implement SRP command buffer using VulkanCommandBuffer
    SRP now uses the device's GetCurrentCommandBuffer() and SuspendDefaultRenderPass() for custom rendering. CmdBindPipeline/CmdDrawIndexed/CmdDrawFullScreenQuad delegate to VulkanCommandBuffer's ICommandBuffer implementation.

--- src/engine/scripting/SRPGraphicsAPI.cpp
+++ src/engine/scripting/SRPGraphicsAPI.cpp
@@ -4,35 +4,35 @@
 #include "graphic/interfaces/IRenderDevice.h"
 #include "graphic/interfaces/IResourceFactory.h"
 #include "graphic/interfaces/IPipelineState.h"
-#include "graphic/ShaderFactory.h"
-#include "graphic/RenderDesc.h"
+#include "graphic/interfaces/ICommandBuffer.h"
 #include "graphic/interfaces/IBuffer.h"
 #include "graphic/interfaces/ITexture.h"
 #include "graphic/interfaces/IRenderTarget.h"
+#include "graphic/ShaderFactory.h"
+#include "graphic/RenderDesc.h"
+#include "adapters/vulkan/RenderDeviceVulkan.h"
 #include "Logger.h"
 
 namespace Prisma::Scripting {
 namespace G = Prisma::Graphic;
+namespace Vk = Prisma::Graphic::Vulkan;
 
-static G::IRenderDevice* GetDevice() {
+static G::IRenderDevice* GetDev() {
     auto* rs = Prisma::Engine::Get().GetRenderSystem();
     return rs ? rs->GetDevice() : nullptr;
 }
-static G::IResourceFactory* GetFactory() {
-    auto* dev = GetDevice();
-    return dev ? dev->GetResourceFactory() : nullptr;
+static G::IResourceFactory* GetFac() {
+    auto* d = GetDev(); return d ? d->GetResourceFactory() : nullptr;
 }
 
 SRPGraphicsAPI& SRPGraphicsAPI::Get() {
-    static SRPGraphicsAPI instance;
-    return instance;
+    static SRPGraphicsAPI inst; return inst;
 }
 
 // === Shader ===
 
-ShaderHandle SRPGraphicsAPI::CreateShader(const char* source, uint32_t sourceLen, SRPShaderStage stage) {
-    auto* device = GetDevice();
-    if (!device) return 0;
+ShaderHandle SRPGraphicsAPI::CreateShader(const char* src, uint32_t len, SRPShaderStage stage) {
+    auto* dev = GetDev(); if (!dev) return 0;
     G::ShaderType t = G::ShaderType::Vertex;
     switch (stage) {
         case SRPShaderStage::Vertex:   t = G::ShaderType::Vertex; break;
@@ -40,16 +40,11 @@ ShaderHandle SRPGraphicsAPI::CreateShader(const char* source, uint32_t sourceLen
         case SRPShaderStage::Compute:  t = G::ShaderType::Compute; break;
         case SRPShaderStage::Geometry: t = G::ShaderType::Geometry; break;
     }
-    G::ShaderDesc desc;
-    desc.type = t;
-    desc.language = G::ShaderLanguage::GLSL;
-    desc.source.assign(source, sourceLen);
-    desc.filename = "srp_shader";
-    auto shader = G::ShaderFactory::CreateShader(device, desc);
-    if (!shader) return 0;
+    G::ShaderDesc sd; sd.type = t; sd.language = G::ShaderLanguage::GLSL;
+    sd.source.assign(src, len); sd.filename = "srp";
+    auto sh = G::ShaderFactory::CreateShader(dev, sd); if (!sh) return 0;
     ShaderHandle h = (ShaderHandle)(m_shaders.size() + 1);
-    m_shaders.push_back(shader);
-    return h;
+    m_shaders.push_back(sh); return h;
 }
 
 void SRPGraphicsAPI::DestroyShader(ShaderHandle h) {
@@ -57,21 +52,18 @@ void SRPGraphicsAPI::DestroyShader(ShaderHandle h) {
 }
 
 std::shared_ptr<G::IShader> SRPGraphicsAPI::GetShaderPtr(ShaderHandle h) {
-    if (h > 0 && h - 1 < m_shaders.size()) return m_shaders[h - 1];
-    return nullptr;
+    return (h > 0 && h - 1 < m_shaders.size()) ? m_shaders[h - 1] : nullptr;
 }
 
 // === Pipeline ===
 
 PipelineHandle SRPGraphicsAPI::CreatePipeline(const SRPPipelineDesc& d) {
-    auto* dev = GetDevice();
-    auto* fac = GetFactory();
+    auto* dev = GetDev(); auto* fac = GetFac();
     if (!dev || !fac) return 0;
-    auto vs = GetShaderPtr(d.vertexShader);
-    auto fs = GetShaderPtr(d.fragmentShader);
+    auto vs = GetShaderPtr(d.vertexShader); auto fs = GetShaderPtr(d.fragmentShader);
     if (!vs || !fs) return 0;
-    auto pso = fac->CreatePipelineStateImpl();
-    if (!pso) return 0;
+
+    auto pso = fac->CreatePipelineStateImpl(); if (!pso) return 0;
     pso->SetShader(G::ShaderType::Vertex, vs);
     pso->SetShader(G::ShaderType::Pixel, fs);
     pso->SetPrimitiveTopology(G::PrimitiveTopology::TriangleList);
@@ -88,46 +80,45 @@ PipelineHandle SRPGraphicsAPI::CreatePipeline(const SRPPipelineDesc& d) {
     pso->SetDepthStencilFormat((G::TextureFormat)d.depthStencilFormat);
     if (!pso->Create(dev)) return 0;
     PipelineHandle h = (PipelineHandle)(m_pipelines.size() + 1);
-    m_pipelines.push_back(std::move(pso));
-    return h;
+    m_pipelines.push_back(std::move(pso)); return h;
 }
 
 void SRPGraphicsAPI::DestroyPipeline(PipelineHandle h) {
     if (h > 0 && h - 1 < m_pipelines.size()) m_pipelines[h - 1].reset();
 }
 
-// === Render Target (TODO) ===
+// === Render Target ===
 
 RenderTargetHandle SRPGraphicsAPI::CreateRenderTarget(int, int, uint32_t, int) { return 0; }
-DepthTargetHandle SRPGraphicsAPI::CreateDepthTarget(int w, int h, uint32_t format) {
-    auto* fac = GetFactory(); if (!fac) return 0;
-    G::TextureDesc td;
-    td.type = G::TextureType::Texture2D;
-    td.format = static_cast<G::TextureFormat>(format);
-    td.width = (uint32_t)w; td.height = (uint32_t)h;
-    td.allowRenderTarget = true;
-    td.allowDepthStencil = true;
-    auto tex = fac->CreateTextureImpl(td);
-    if (!tex) return 0;
+
+DepthTargetHandle SRPGraphicsAPI::CreateDepthTarget(int w, int h, uint32_t fmt) {
+    auto* fac = GetFac(); if (!fac) return 0;
+    G::TextureDesc td; td.type = G::TextureType::Texture2D; td.format = (G::TextureFormat)fmt;
+    td.width = (uint32_t)w; td.height = (uint32_t)h; td.allowRenderTarget = true; td.allowDepthStencil = true;
+    auto tex = fac->CreateTextureImpl(td); if (!tex) return 0;
     DepthTargetHandle hdl = (DepthTargetHandle)(m_depthTargets.size() + 1);
-    m_depthTargets.push_back(std::shared_ptr<G::ITexture>(std::move(tex)));
-    return hdl;
+    m_depthTargets.push_back(std::move(tex)); return hdl;
 }
-void SRPGraphicsAPI::DestroyRenderTarget(RenderTargetHandle) {}
-void SRPGraphicsAPI::DestroyDepthTarget(DepthTargetHandle) {}
+
+void SRPGraphicsAPI::DestroyRenderTarget(RenderTargetHandle h) {
+    if (h > 0 && h - 1 < m_renderTargets.size()) m_renderTargets[h - 1].reset();
+}
+void SRPGraphicsAPI::DestroyDepthTarget(DepthTargetHandle h) {
+    if (h > 0 && h - 1 < m_depthTargets.size()) m_depthTargets[h - 1].reset();
+}
 
 // === Buffers ===
 
 BufferHandle SRPGraphicsAPI::CreateVertexBuffer(const void* data, uint32_t size, uint32_t stride) {
-    auto* fac = GetFactory(); if (!fac) return 0;
+    auto* fac = GetFac(); if (!fac) return 0;
     G::BufferDesc bd; bd.type = G::BufferType::Vertex; bd.size = size; bd.stride = stride; bd.initialData = data; bd.usage = G::BufferUsage::Immutable;
     auto buf = fac->CreateBufferImpl(bd); if (!buf) return 0;
     BufferHandle h = (BufferHandle)(m_buffers.size() + 1); m_buffers.push_back(std::move(buf)); return h;
 }
 
-BufferHandle SRPGraphicsAPI::CreateIndexBuffer(const void* data, uint32_t size, bool is32Bit) {
-    auto* fac = GetFactory(); if (!fac) return 0;
-    G::BufferDesc bd; bd.type = G::BufferType::Index; bd.size = size; bd.stride = is32Bit ? 4 : 2; bd.initialData = data; bd.usage = G::BufferUsage::Immutable;
+BufferHandle SRPGraphicsAPI::CreateIndexBuffer(const void* data, uint32_t size, bool is32) {
+    auto* fac = GetFac(); if (!fac) return 0;
+    G::BufferDesc bd; bd.type = G::BufferType::Index; bd.size = size; bd.stride = is32 ? 4 : 2; bd.initialData = data; bd.usage = G::BufferUsage::Immutable;
     auto buf = fac->CreateBufferImpl(bd); if (!buf) return 0;
     BufferHandle h = (BufferHandle)(m_buffers.size() + 1); m_buffers.push_back(std::move(buf)); return h;
 }
@@ -136,37 +127,80 @@ void SRPGraphicsAPI::DestroyBuffer(BufferHandle h) {
     if (h > 0 && h - 1 < m_buffers.size()) m_buffers[h - 1].reset();
 }
 
-// === Texture (TODO) ===
+// === Texture (stub) ===
 
 TextureHandle SRPGraphicsAPI::CreateTexture2D(int, int, uint32_t, const void*, uint32_t) { return 0; }
 void SRPGraphicsAPI::DestroyTexture(TextureHandle) {}
 
-// === Frame ===
-void SRPGraphicsAPI::BeginFrame() {}
-void SRPGraphicsAPI::EndFrame() {}
+// === Frame & Command Recording ===
 
-// === Command Buffer (stubs - will use srpRender callback instead) ===
-void SRPGraphicsAPI::CmdBeginRenderPass(uint32_t, const uint32_t*, uint32_t, const float*, float, int, int) {}
-void SRPGraphicsAPI::CmdEndRenderPass() {}
-void SRPGraphicsAPI::CmdBindPipeline(PipelineHandle) {}
-void SRPGraphicsAPI::CmdBindVertexBuffer(BufferHandle, uint32_t, uint32_t) {}
-void SRPGraphicsAPI::CmdBindIndexBuffer(BufferHandle, uint32_t, int) {}
-void SRPGraphicsAPI::CmdSetViewport(int, int, int, int) {}
-void SRPGraphicsAPI::CmdSetScissor(int, int, int, int) {}
-void SRPGraphicsAPI::CmdPushConstants(uint32_t, uint32_t, const void*) {}
-void SRPGraphicsAPI::CmdDraw(uint32_t, uint32_t, uint32_t) {}
-void SRPGraphicsAPI::CmdDrawIndexed(uint32_t, uint32_t, uint32_t, int32_t) {}
-void SRPGraphicsAPI::CmdDrawFullScreenQuad() {}
-
-void SRPGraphicsAPI::Shutdown() {
-    m_shaders.clear(); m_pipelines.clear();
-    m_renderTargets.clear(); m_depthTargets.clear();
-    m_buffers.clear(); m_textures.clear();
+void SRPGraphicsAPI::BeginFrame() {
+    auto* dev = GetDev();
+    if (!dev) { m_cmdBuffer = nullptr; return; }
+    // Get device's current command buffer (recording after BeginFrame)
+    auto* vkDev = dynamic_cast<Vk::RenderDeviceVulkan*>(dev);
+    if (vkDev) {
+        vkDev->SuspendDefaultRenderPass();
+        m_cmdBuffer = vkDev->GetCurrentCommandBuffer();
+    }
 }
 
-// ===== C-callable wrappers =====
+void SRPGraphicsAPI::EndFrame() {
+    auto* dev = dynamic_cast<Vk::RenderDeviceVulkan*>(GetDev());
+    if (dev && m_cmdBuffer) {
+        dev->ResumeDefaultRenderPass();
+    }
+    m_cmdBuffer = nullptr;
+}
 
-uint32_t SRP_CreateShader(const char* s, uint32_t len, uint32_t stage) { return SRPGraphicsAPI::Get().CreateShader(s, len, (SRPShaderStage)stage); }
+void SRPGraphicsAPI::CmdBeginRenderPass(uint32_t, const uint32_t*, uint32_t, const float*, float, int, int) {
+    if (!m_cmdBuffer) return;
+    G::RenderPassDesc desc;
+    desc.renderTarget = nullptr; // TODO: lookup RT handle
+    desc.clearRenderTarget = false;
+    desc.renderArea = {0, 0, 0, 0};
+    m_cmdBuffer->BeginRenderPass(desc);
+}
+
+void SRPGraphicsAPI::CmdEndRenderPass() { if (m_cmdBuffer) m_cmdBuffer->EndRenderPass(); }
+void SRPGraphicsAPI::CmdBindPipeline(PipelineHandle h) {
+    if (!m_cmdBuffer || !(h > 0 && h - 1 < m_pipelines.size())) return;
+    m_cmdBuffer->SetPipelineState(m_pipelines[h - 1].get());
+}
+void SRPGraphicsAPI::CmdBindVertexBuffer(BufferHandle h, uint32_t s, uint32_t o) {
+    if (!m_cmdBuffer || !(h > 0 && h - 1 < m_buffers.size())) return;
+    m_cmdBuffer->SetVertexBuffer(m_buffers[h - 1].get(), s, o);
+}
+void SRPGraphicsAPI::CmdBindIndexBuffer(BufferHandle h, uint32_t o, int is32) {
+    if (!m_cmdBuffer || !(h > 0 && h - 1 < m_buffers.size())) return;
+    m_cmdBuffer->SetIndexBuffer(m_buffers[h - 1].get(), is32 != 0, o);
+}
+void SRPGraphicsAPI::CmdSetViewport(int x, int y, int w, int h) {
+    if (!m_cmdBuffer) return;
+    G::Viewport vp; vp.x = (float)x; vp.y = (float)y; vp.width = (float)w; vp.height = (float)h; vp.minDepth = 0; vp.maxDepth = 1;
+    m_cmdBuffer->SetViewport(vp);
+}
+void SRPGraphicsAPI::CmdSetScissor(int x, int y, int w, int h) {
+    if (!m_cmdBuffer) return;
+    G::Rect r; r.x = x; r.y = y; r.width = w; r.height = h;
+    m_cmdBuffer->SetScissorRect(r);
+}
+void SRPGraphicsAPI::CmdPushConstants(uint32_t o, uint32_t sz, const void* d) { if (m_cmdBuffer) m_cmdBuffer->PushConstants(G::ShaderType::Vertex, d, sz); }
+void SRPGraphicsAPI::CmdDraw(uint32_t vc, uint32_t ic, uint32_t fv) { if (m_cmdBuffer) m_cmdBuffer->Draw(vc, ic, fv); }
+void SRPGraphicsAPI::CmdDrawIndexed(uint32_t ic, uint32_t instc, uint32_t fi, int32_t vo) { if (m_cmdBuffer) m_cmdBuffer->DrawIndexed(ic, instc, fi, vo); }
+void SRPGraphicsAPI::CmdDrawFullScreenQuad() { if (m_cmdBuffer) m_cmdBuffer->Draw(4, 1, 0); }
+
+void SRPGraphicsAPI::Shutdown() {
+    m_shaders.clear(); m_pipelines.clear(); m_renderTargets.clear();
+    m_depthTargets.clear(); m_buffers.clear(); m_textures.clear();
+    m_cmdBuffer = nullptr;
+}
+
+// ===== C Wrappers =====
+
+#define C0(name) void SRP_##name() { SRPGraphicsAPI::Get().name(); }
+
+uint32_t SRP_CreateShader(const char* s, uint32_t l, uint32_t st) { return SRPGraphicsAPI::Get().CreateShader(s, l, (SRPShaderStage)st); }
 void SRP_DestroyShader(uint32_t h) { SRPGraphicsAPI::Get().DestroyShader(h); }
 uint32_t SRP_CreatePipeline(const SRPPipelineDesc* d) { return d ? SRPGraphicsAPI::Get().CreatePipeline(*d) : 0; }
 void SRP_DestroyPipeline(uint32_t h) { SRPGraphicsAPI::Get().DestroyPipeline(h); }
@@ -181,9 +215,8 @@ uint32_t SRP_CreateTexture2D(int w, int h, uint32_t f, const void* p, uint32_t p
 void SRP_DestroyTexture(uint32_t h) { SRPGraphicsAPI::Get().DestroyTexture(h); }
 void SRP_BeginFrame() { SRPGraphicsAPI::Get().BeginFrame(); }
 void SRP_EndFrame() { SRPGraphicsAPI::Get().EndFrame(); }
-
 void SRP_CmdBeginRenderPass(uint32_t rc, const uint32_t* rh, uint32_t dh, const float* cc, float dc, int vw, int vh) { SRPGraphicsAPI::Get().CmdBeginRenderPass(rc, rh, dh, cc, dc, vw, vh); }
-void SRP_CmdEndRenderPass() { SRPGraphicsAPI::Get().CmdEndRenderPass(); }
+C0(CmdEndRenderPass)
 void SRP_CmdBindPipeline(uint32_t h) { SRPGraphicsAPI::Get().CmdBindPipeline(h); }
 void SRP_CmdBindVertexBuffer(uint32_t h, uint32_t s, uint32_t o) { SRPGraphicsAPI::Get().CmdBindVertexBuffer(h, s, o); }
 void SRP_CmdBindIndexBuffer(uint32_t h, uint32_t o, int is32) { SRPGraphicsAPI::Get().CmdBindIndexBuffer(h, o, is32); }
@@ -193,7 +226,6 @@ void SRP_CmdPushConstants(uint32_t o, uint32_t sz, const void* d) { SRPGraphicsA
 void SRP_CmdDraw(uint32_t vc, uint32_t ic, uint32_t fv) { SRPGraphicsAPI::Get().CmdDraw(vc, ic, fv); }
 void SRP_CmdDrawIndexed(uint32_t ic, uint32_t instc, uint32_t fi, int32_t vo) { SRPGraphicsAPI::Get().CmdDrawIndexed(ic, instc, fi, vo); }
 void SRP_CmdDrawFullScreenQuad() { SRPGraphicsAPI::Get().CmdDrawFullScreenQuad(); }
-
 void SRP_Shutdown() { SRPGraphicsAPI::Get().Shutdown(); }
 
 } // namespace Prisma::Scripting
--- src/engine/scripting/SRPGraphicsAPI.h
+++ src/engine/scripting/SRPGraphicsAPI.h
@@ -10,16 +10,12 @@ namespace Prisma::Graphic {
     class IRenderTarget;
     class IBuffer;
     class ITexture;
-
+    class ICommandBuffer;
 }
 
 namespace Prisma::Scripting {
 
-// ============================================================================
-// SRP Graphics API - Thin C wrapper around engine rendering for C# interop
-// ============================================================================
-
-// Handle types (uint32_t, 0 = invalid)
+// Handle types
 using ShaderHandle = uint32_t;
 using PipelineHandle = uint32_t;
 using RenderTargetHandle = uint32_t;
@@ -27,77 +23,65 @@ using DepthTargetHandle = uint32_t;
 using BufferHandle = uint32_t;
 using TextureHandle = uint32_t;
 
-// C-compatible struct for pipeline creation from C#
+// Pipeline creation desc - binary compatible with C#
 struct SRPPipelineDesc {
-    ShaderHandle vertexShader;
-    ShaderHandle fragmentShader;
+    uint32_t vertexShader;
+    uint32_t fragmentShader;
     uint32_t numRenderTargets;
-    uint32_t renderTargetFormats[8];  // TextureFormat enum values
-    uint32_t depthStencilFormat;       // TextureFormat enum value
+    uint32_t renderTargetFormats[8];
+    uint32_t depthStencilFormat;
     uint32_t sampleCount;
-    uint32_t cullMode;                 // 0=none, 1=front, 2=back
-    bool depthTest;
-    bool depthWrite;
-    uint8_t depthFunc;                 // 4=less (D3D12 default)
-    bool blendEnable;
-    uint8_t blendColorWriteMask;       // 0xF = RGBA
-    float clearColor[4];
+    uint32_t cullMode;
+    uint8_t  depthTest;
+    uint8_t  depthWrite;
+    uint8_t  depthFunc;
+    uint8_t  blendEnable;
+    uint8_t  blendColorWriteMask;
+    float    clearColor[4];
 };
 
-// Shader stage enum
+// Shader stage
 enum class SRPShaderStage : uint32_t {
-    Vertex = 0,
-    Fragment = 1,
-    Compute = 2,
-    Geometry = 3
+    Vertex = 0, Fragment = 1, Compute = 2, Geometry = 3
 };
 
 // ============================================================================
-// SRP Graphics API - singleton controller
+// SRP Graphics API
 // ============================================================================
 class SRPGraphicsAPI {
 public:
     static SRPGraphicsAPI& Get();
 
-    // --- Shader ---
     ShaderHandle CreateShader(const char* source, uint32_t sourceLen, SRPShaderStage stage);
-    void DestroyShader(ShaderHandle handle);
-    std::shared_ptr<Graphic::IShader> GetShaderPtr(ShaderHandle handle);
+    void DestroyShader(ShaderHandle h);
+    std::shared_ptr<Graphic::IShader> GetShaderPtr(ShaderHandle h);
 
-    // --- Pipeline ---
     PipelineHandle CreatePipeline(const SRPPipelineDesc& desc);
-    void DestroyPipeline(PipelineHandle handle);
-    Graphic::IPipelineState* GetPipeline(PipelineHandle handle);
+    void DestroyPipeline(PipelineHandle h);
 
-    // --- Render Target ---
-    RenderTargetHandle CreateRenderTarget(int width, int height, uint32_t format, int samples);
-    DepthTargetHandle CreateDepthTarget(int width, int height, uint32_t format);
-    void DestroyRenderTarget(RenderTargetHandle handle);
-    void DestroyDepthTarget(DepthTargetHandle handle);
-    Graphic::IRenderTarget* GetRenderTarget(RenderTargetHandle handle);
-    Graphic::ITexture* GetDepthTarget(DepthTargetHandle handle);
+    RenderTargetHandle CreateRenderTarget(int w, int h, uint32_t format, int samples);
+    DepthTargetHandle CreateDepthTarget(int w, int h, uint32_t format);
+    void DestroyRenderTarget(RenderTargetHandle h);
+    void DestroyDepthTarget(DepthTargetHandle h);
 
-    // --- Vertex/Index Buffer ---
     BufferHandle CreateVertexBuffer(const void* data, uint32_t size, uint32_t stride);
     BufferHandle CreateIndexBuffer(const void* data, uint32_t size, bool is32Bit);
-    void DestroyBuffer(BufferHandle handle);
-    Graphic::IBuffer* GetBuffer(BufferHandle handle);
+    void DestroyBuffer(BufferHandle h);
 
-    // --- Texture (2D) ---
-    TextureHandle CreateTexture2D(int width, int height, uint32_t format, const void* pixels, uint32_t pixelSize);
-    void DestroyTexture(TextureHandle handle);
-    Graphic::ITexture* GetTexture(TextureHandle handle);
+    TextureHandle CreateTexture2D(int w, int h, uint32_t format, const void* pixels, uint32_t pixelSize);
+    void DestroyTexture(TextureHandle h);
 
-    // --- Per-frame state ---
+    // Frame lifecycle - uses device's current command buffer
     void BeginFrame();
     void EndFrame();
 
-    // --- Command Buffer Methods (called between BeginFrame/EndFrame) ---
-    void CmdBeginRenderPass(uint32_t rtCount, const uint32_t* rtHandles, uint32_t depthHandle, const float* clearColors, float depthClear, int viewW, int viewH);
+    // Command recording (between BeginFrame/EndFrame)
+    void CmdBeginRenderPass(uint32_t rtCount, const uint32_t* rtHandles, uint32_t depthHandle,
+                            const float* clearColors, float depthClear, int viewW, int viewH);
     void CmdEndRenderPass();
-    void CmdBindPipeline(PipelineHandle handle);
-    void CmdBindVertexBuffer(BufferHandle handle, uint32_t slot, uint32_t offset);
-    void CmdBindIndexBuffer(BufferHandle handle, uint32_t offset, int is32Bit);
+    void CmdBindPipeline(PipelineHandle h);
+    void CmdBindVertexBuffer(BufferHandle h, uint32_t slot, uint32_t offset);
+    void CmdBindIndexBuffer(BufferHandle h, uint32_t offset, int is32Bit);
     void CmdSetViewport(int x, int y, int w, int h);
     void CmdSetScissor(int x, int y, int w, int h);
     void CmdPushConstants(uint32_t offset, uint32_t size, const void* data);
@@ -105,7 +89,6 @@ public:
     void CmdDrawIndexed(uint32_t indexCount, uint32_t instanceCount, uint32_t firstIndex, int32_t vertexOffset);
     void CmdDrawFullScreenQuad();
 
-    // --- Cleanup ---
     void Shutdown();
 
 private:
@@ -117,56 +100,40 @@ private:
     std::vector<std::shared_ptr<Graphic::ITexture>> m_depthTargets;
     std::vector<std::shared_ptr<Graphic::IBuffer>> m_buffers;
     std::vector<std::shared_ptr<Graphic::ITexture>> m_textures;
+
+    // Current frame command buffer (not owned - from device)
+    Graphic::ICommandBuffer* m_cmdBuffer = nullptr;
 };
 
-// ============================================================================
-// C-callable function pointers for PrismaAPI
-// ============================================================================
+// C-callable wrappers for PrismaAPI
 extern "C" {
-
-// Shader
-uint32_t SRP_CreateShader(const char* source, uint32_t sourceLen, uint32_t stage);
-void SRP_DestroyShader(uint32_t handle);
-
-// Pipeline
-uint32_t SRP_CreatePipeline(const SRPPipelineDesc* desc);
-void SRP_DestroyPipeline(uint32_t handle);
-
-// Render Target
-uint32_t SRP_CreateRenderTarget(int w, int h, uint32_t format, int samples);
-uint32_t SRP_CreateDepthTarget(int w, int h, uint32_t format);
-void SRP_DestroyRenderTarget(uint32_t handle);
-void SRP_DestroyDepthTarget(uint32_t handle);
-
-// Buffers
-uint32_t SRP_CreateVertexBuffer(const void* data, uint32_t size, uint32_t stride);
-uint32_t SRP_CreateIndexBuffer(const void* data, uint32_t size, int is32Bit);
-void SRP_DestroyBuffer(uint32_t handle);
-
-// Texture
-uint32_t SRP_CreateTexture2D(int w, int h, uint32_t format, const void* pixels, uint32_t pixelSize);
-void SRP_DestroyTexture(uint32_t handle);
-
-// Frame commands
-void SRP_BeginFrame();
-void SRP_EndFrame();
-
-// Command buffer (called between BeginFrame/EndFrame)
-void SRP_CmdBeginRenderPass(uint32_t rtCount, const uint32_t* rtHandles, uint32_t depthHandle, const float* clearColors, float depthClear, int viewW, int viewH);
-void SRP_CmdEndRenderPass();
-void SRP_CmdBindPipeline(uint32_t handle);
-void SRP_CmdBindVertexBuffer(uint32_t handle, uint32_t slot, uint32_t offset);
-void SRP_CmdBindIndexBuffer(uint32_t handle, uint32_t offset, int is32Bit);
-void SRP_CmdSetViewport(int x, int y, int w, int h);
-void SRP_CmdSetScissor(int x, int y, int w, int h);
-void SRP_CmdPushConstants(uint32_t offset, uint32_t size, const void* data);
-void SRP_CmdDraw(uint32_t vertexCount, uint32_t instanceCount, uint32_t firstVertex);
-void SRP_CmdDrawIndexed(uint32_t indexCount, uint32_t instanceCount, uint32_t firstIndex, int32_t vertexOffset);
-void SRP_CmdDrawFullScreenQuad();
-
-// Cleanup
-void SRP_Shutdown();
-
-} // extern "C"
+    uint32_t SRP_CreateShader(const char* s, uint32_t len, uint32_t stage);
+    void SRP_DestroyShader(uint32_t h);
+    uint32_t SRP_CreatePipeline(const SRPPipelineDesc* d);
+    void SRP_DestroyPipeline(uint32_t h);
+    uint32_t SRP_CreateRenderTarget(int w, int h, uint32_t f, int s);
+    uint32_t SRP_CreateDepthTarget(int w, int h, uint32_t f);
+    void SRP_DestroyRenderTarget(uint32_t h);
+    void SRP_DestroyDepthTarget(uint32_t h);
+    uint32_t SRP_CreateVertexBuffer(const void* d, uint32_t sz, uint32_t st);
+    uint32_t SRP_CreateIndexBuffer(const void* d, uint32_t sz, int is32);
+    void SRP_DestroyBuffer(uint32_t h);
+    uint32_t SRP_CreateTexture2D(int w, int h, uint32_t f, const void* p, uint32_t ps);
+    void SRP_DestroyTexture(uint32_t h);
+    void SRP_BeginFrame();
+    void SRP_EndFrame();
+    void SRP_CmdBeginRenderPass(uint32_t rc, const uint32_t* rh, uint32_t dh, const float* cc, float dc, int vw, int vh);
+    void SRP_CmdEndRenderPass();
+    void SRP_CmdBindPipeline(uint32_t h);
+    void SRP_CmdBindVertexBuffer(uint32_t h, uint32_t s, uint32_t o);
+    void SRP_CmdBindIndexBuffer(uint32_t h, uint32_t o, int is32);
+    void SRP_CmdSetViewport(int x, int y, int w, int h);
+    void SRP_CmdSetScissor(int x, int y, int w, int h);
+    void SRP_CmdPushConstants(uint32_t o, uint32_t sz, const void* d);
+    void SRP_CmdDraw(uint32_t vc, uint32_t ic, uint32_t fv);
+    void SRP_CmdDrawIndexed(uint32_t ic, uint32_t instc, uint32_t fi, int32_t vo);
+    void SRP_CmdDrawFullScreenQuad();
+    void SRP_Shutdown();
+}
 
 } // namespace Prisma::Scripting
